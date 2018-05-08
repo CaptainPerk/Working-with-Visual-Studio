567795989ba3eead59ef0fab157163a910286d2d
Jon Perkins <captainperk81@gmail.com>
2018-05-08 19:41:45 -0400

    Enabling Static Content Delivery

--- WorkingWithVisualStudio/Startup.cs
+++ WorkingWithVisualStudio/Startup.cs
@@ -18,6 +18,7 @@ namespace WorkingWithVisualStudio
         {
             app.UseDeveloperExceptionPage();
             app.UseBrowserLink();
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
