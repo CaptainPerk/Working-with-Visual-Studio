9a8716b78fff27b2a583598f222634e00a4fd879
Jon Perkins <captainperk81@gmail.com>
2018-05-09 18:54:17 -0400

    Isolating Componets for Unit Testing Part 1

---
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model, 
+                Comparer.Get<Product>((product1, product2) => product1.Name == product2.Name && product1.Price == product2.Price));
+        }
+    }
+}
